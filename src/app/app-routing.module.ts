5b623ba36c39868d639bdaf25a9a19e8dfa91726
damd0609 <danygomezs36@gmail.com>
2021-03-16 14:39:50 -0500

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivasComponent } from './directivas/directivas.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
+import { PaginaerrorComponent } from './paginaerror/paginaerror.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { TitulosComponent } from './titulos/titulos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: TitulosComponent},
+  {path:'pipes', component: PipesComponent},
+  {path:'ejemplo', component: EjemploComponent},
+  {path:'directivas', component: DirectivasComponent},
+  {path:'**', component: PaginaerrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitulosComponent } from './titulos/titulos.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
+import { DirectivasComponent } from './directivas/directivas.component';
+import { PaginaerrorComponent } from './paginaerror/paginaerror.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { PiepaginaComponent } from './piepagina/piepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitulosComponent,
+    PipesComponent,
+    EjemploComponent,
+    DirectivasComponent,
+    PaginaerrorComponent,
+    EncabezadoComponent,
+    PiepaginaComponent
   ],
   imports: [
     BrowserModule,
